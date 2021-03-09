4b33779346e2e3a821be99731fc0c678ac32d19c
vaishali161197 <vaishali161197vs@gmail.com>
2021-03-09 18:08:25 +0530

    Rocket Movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float RocketSpeed = 100f;
+    [SerializeField] float RocketRotation = 1f;
+    Rigidbody Rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Rb.AddRelativeForce(Vector3.up * Time.deltaTime * RocketSpeed);
+        }
+
+    }
+
+    private void ProcessRotation()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(RocketRotation); 
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-RocketRotation);
+
+        }
+    }
+
+    private void ApplyRotation(float rocketRotation)
+    {
+        Rb.freezeRotation = true; //freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * Time.deltaTime * rocketRotation);
+        Rb.freezeRotation = false; //unfreezing rotation so the physics system takeover
+    }
+
+
+
+
+}
