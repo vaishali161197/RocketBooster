dc5e4478809728fd1fb68248a8b04d54990e420a
vaishali161197 <vaishali161197vs@gmail.com>
2021-03-11 16:34:56 +0530

    LoadNextLevel

---
+++ Assets/Scripts/RocketCollision.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RocketCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log(" You Bumped into launching pad ");
+                break;
+
+            case "Finish":
+                LoadNextLevel();
+                break;
+
+            case "Fuel":
+                Debug.Log(" You picked a fuel ");
+                break;
+
+            default:
+                ReloadLevel();
+                break;
+        }
+
+
+    }
+
+    private void LoadNextLevel()
+    {
+        int NextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if(NextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            NextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(NextSceneIndex);
+    }
+
+    private void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
